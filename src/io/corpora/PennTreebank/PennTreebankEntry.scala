55486e8f174a10e75df24c4d7150353d18a5538d
yuhuan <jyuhuan@gmail.com>
2015-03-10 22:51:31 -0400

    added comment for corpora.Penntreebank.PennTreebank object

--- src/io/corpora/PennTreebank/PennTreebankEntry.scala
+++ src/io/corpora/PennTreebank/PennTreebankEntry.scala
@@ -4,6 +4,11 @@ import foundation.math.graph.Tree
 /**
  * Created by Yuhuan Jiang (jyuhuan@gmail.com) on 3/10/15.
  */
+
+/**
+ *
+ * @param tree
+ */
 case class PennTreebankEntry(tree: Tree[String]) {
   val sentence = tree.leaves().mkString(" ")
   override def toString() = sentence
