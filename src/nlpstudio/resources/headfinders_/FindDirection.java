1f71749ca3e0344c6858d9625ad00e1b993201b4
yuhuan <jyuhuan@gmail.com>
2015-03-22 20:38:35 -0400

    fix repo

---
+++ src/nlpstudio/resources/headfinders_/FindDirection.java
@@ -0,0 +1,9 @@
+package nlpstudio.resources.HeadFinders;
+
+/**
+ * Created by Yuhuan Jiang (jyuhuan@gmail.com) on 3/22/15.
+ */
+public enum FindDirection {
+    RightToLeft,
+    LeftToRight
+}
---
+++ src/nlpstudio/resources/headfinders_/HeadFinder.scala
@@ -0,0 +1,11 @@
+package nlpstudio.resources.HeadFinders
+
+import nlpstudio.resources.penntreebank.PennTreebankNode
+
+/**
+ * Created by Yuhuan Jiang (jyuhuan@gmail.com) on 3/22/15.
+ */
+trait HeadFinder {
+  def find(ptbNode: PennTreebankNode): PennTreebankNode
+  def apply(ptbNode: PennTreebankNode): PennTreebankNode = find(ptbNode)
+}
