de4141f17a56554b85f8a799e3985192f0cf2a42
yuhuan <jyuhuan@gmail.com>
2015-03-24 22:15:23 -0400

    Changed the string representation for class Rule

--- src/nlpstudio/resources/core/Rule.scala
+++ src/nlpstudio/resources/core/Rule.scala
@@ -4,5 +4,5 @@ package nlpstudio.resources.core
  * Created by yuhuan on 3/21/15.
  */
 case class Rule(head: String, body: Seq[String]) {
-  override def toString = head + " -> " + body.mkString(" ")
+  override def toString = head + " → " + body.mkString(" ")
 }
