a4b46e376b79ee29095f9bbcf732055b8d376bc2
yuhuan <jyuhuan@gmail.com>
2015-03-24 22:13:38 -0400

    removed old feature vector implementation (which was empty anyway), added a new one.

--- src/learning/features/Feature.scala
+++
@@ -1,6 +0,0 @@
-package learning.features
-
-/**
- * Created by Yuhuan Jiang (jyuhuan@gmail.com) on 3/23/15.
- */
-case class Feature(name: String, value: String)
--- src/learning/features/Featurizer.scala
+++
@@ -1,8 +0,0 @@
-package learning.features
-
-/**
- * Created by Yuhuan Jiang (jyuhuan@gmail.com) on 3/23/15.
- */
-class Featurizer[T] {
-  
-}
