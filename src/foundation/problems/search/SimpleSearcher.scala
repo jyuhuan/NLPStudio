67d46a24e049131926ae30db5ff712f4d4d89ae5
yuhuan <jyuhuan@gmail.com>
2015-03-25 23:31:43 -0400

    added SimpleSearcher different from the previous one (Searcher) in that, this searcher does not require user to manipulate SearchNode[TState, TAction]

--- src/foundation/problems/search/SimpleSearcher.scala
+++ src/foundation/problems/search/SimpleSearcher.scala
@@ -10,26 +10,30 @@ import foundation.sugar.OrderedCollectionClasses._
  * Created by Yuhuan Jiang (jyuhuan@gmail.com) on 3/25/15.
  */
 
-case class SimpleSearchNode[TState](state: TState, parent: SimpleSearchNode[TState])
+case class SimpleSearchNode[S](state: S, parent: SimpleSearchNode[S])
 
 object SimpleSearcher {
 
-  def depthFirstSearch[TState](startState: TState, isGoal: TState ⇒ Boolean, generateSuccessors: TState ⇒ Iterable[TState]): TState = {
-    basicGraphSearch(startState, isGoal, generateSuccessors, new mutable.Stack[SimpleSearchNode[TState]]())
+  def depthFirstSearch[S](startState: S,
+                               isGoal: S ⇒ Boolean,
+                               generateSuccessors: S ⇒ Iterable[S]): S = {
+    basicGraphSearch(startState, isGoal, generateSuccessors, new mutable.Stack[SimpleSearchNode[S]]())
   }
 
-  def breadthFirstSearch[TState](startState: TState, isGoal: TState ⇒ Boolean, generateSuccessors: TState ⇒ Iterable[TState]): TState = {
-    basicGraphSearch(startState, isGoal, generateSuccessors, new mutable.Queue[SimpleSearchNode[TState]]())
+  def breadthFirstSearch[S](startState: S,
+                                 isGoal: S ⇒ Boolean,
+                                 generateSuccessors: S ⇒ Iterable[S]): S = {
+    basicGraphSearch(startState, isGoal, generateSuccessors, new mutable.Queue[SimpleSearchNode[S]]())
   }
 
-  private def basicGraphSearch[TState](startState: TState,
-                                       isGoal: TState ⇒ Boolean,
-                                       generateSuccessors: TState ⇒ Iterable[TState],
-                                       fringe: OrderedCollection[SimpleSearchNode[TState]]): TState = {
-    val startNode = SimpleSearchNode[TState](startState, null)
+  private def basicGraphSearch[S](startState: S,
+                                       isGoal: S ⇒ Boolean,
+                                       generateSuccessors: S ⇒ Iterable[S],
+                                       fringe: OrderedCollection[SimpleSearchNode[S]]): S = {
+    val startNode = SimpleSearchNode[S](startState, null)
     fringe.enqueue(startNode)
-    val explored = mutable.HashSet[TState]()
-    var goalSearchNode: SimpleSearchNode[TState] = null
+    val explored = mutable.HashSet[S]()
+    var goalSearchNode: SimpleSearchNode[S] = null
     while (goalSearchNode == null && fringe.notEmpty()) {
       val top = fringe.dequeue()
       explored.add(top.state)
@@ -38,11 +42,7 @@ object SimpleSearcher {
       }
       fringe.enqueueAll(generateSuccessors(top.state).filter(s ⇒ !explored.contains(s)).map(x => SimpleSearchNode(x, top)))
     }
-    if (goalSearchNode != null) {
-      goalSearchNode.state
-    }
-    else {
-      throw new GoalNotFoundException("The search fails to find the goal. ")
-    }
+    if (goalSearchNode != null) goalSearchNode.state
+    else throw new GoalNotFoundException("The search fails to find the goal. ")
   }
 }
