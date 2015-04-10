0f3aca22681f2c7abf5aa429e29d22e9460d8eef
yuhuan <jyuhuan@gmail.com>
2015-04-09 23:00:51 -0400

    added subjectivity resource

---
+++ src/nlpstudio/resources/subjectivelexicons/SubjectivityIntensity.java
@@ -0,0 +1,9 @@
+package nlpstudio.resources.subjectivelexicons;
+
+/**
+ * Created by Yuhuan Jiang (jyuhuan@gmail.com) on 4/3/15.
+ */
+public enum SubjectivityIntensity {
+    Strong,
+    Weak,
+}
---
+++ src/nlpstudio/resources/subjectivelexicons/SubjectivityLexicon.scala
@@ -0,0 +1,8 @@
+package nlpstudio.resources.subjectivelexicons
+
+/**
+ * Created by Yuhuan Jiang (jyuhuan@gmail.com) on 4/3/15.
+ */
+trait SubjectivityLexicon {
+
+}
