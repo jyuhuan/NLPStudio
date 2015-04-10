7e62520d54efc5888fab14102779be289534a7ef
yuhuan <jyuhuan@gmail.com>
2015-04-09 23:03:31 -0400

    change name of Direcotry.scala

--- src/nlpstudio/io/files/Directory.scala
+++ src/nlpstudio/io/files/Directory.scala
@@ -3,7 +3,7 @@ package nlpstudio.io.files
 /**
  * Created by Yuhuan Jiang (jyuhuan@gmail.com) on 3/11/15.
  */
-object directory {
+object Directory {
   def allFiles(pathToDirectory: String): Traversable[java.io.File] = {
     new java.io.File(pathToDirectory).listFiles()
   }
