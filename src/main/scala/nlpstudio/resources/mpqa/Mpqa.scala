39dc0a6c6d36c151900d3ac65f2465a80103b2f5
yuhuan <jyuhuan@gmail.com>
2015-05-15 01:23:17 -0400

    start to read MPQA corpus

--- src/main/scala/nlpstudio/resources/mpqa/Mpqa.scala
+++ src/main/scala/nlpstudio/resources/mpqa/Mpqa.scala
@@ -4,5 +4,9 @@ package nlpstudio.resources.mpqa
  * Created by Yuhuan Jiang (jyuhuan@gmail.com) on 5/11/15.
  */
 object Mpqa {
-  def load(path: String): Object = null
+  def load(path: String): Object = loadSentencesAnnotatedWithETargets(path)
+
+  def loadSentencesAnnotatedWithETargets(path: String): Object = {
+    null
+  }
 }
