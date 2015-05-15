b1121744551c8cd5a509e4707be85e6f2f55ef57
yuhuan <jyuhuan@gmail.com>
2015-05-15 01:22:36 -0400

    wrapper for stanford DocumentPreprocessor

---
+++ src/main/scala/nlpstudio/tools/tokenizers/StanfordTokenizer.scala
@@ -0,0 +1,18 @@
+package nlpstudio.tools.tokenizers
+
+import java.io.StringReader
+import nlpstudio.core.Word
+
+import scala.collection.JavaConversions._
+import edu.stanford.nlp.process.DocumentPreprocessor
+
+/**
+ * Created by Yuhuan Jiang (jyuhuan@gmail.com) on 5/15/15.
+ */
+object StanfordTokenizer {
+  def tokenize(sentence: String): Array[Word] = {
+    val stringReader = new StringReader(sentence)
+    val documentPreprocessor = new DocumentPreprocessor(stringReader)
+    documentPreprocessor.head.map(x ⇒ new Word(x.word())).toArray
+  }
+}
