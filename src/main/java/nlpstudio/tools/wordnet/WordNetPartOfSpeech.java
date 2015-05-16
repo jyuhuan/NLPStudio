92f601f158672491babfa9dea685dcdf290359ed
yuhuan <jyuhuan@gmail.com>
2015-05-16 01:51:16 -0400

    added wordnet support (can look up hypo, hyper and syn now)

---
+++ src/main/java/nlpstudio/tools/wordnet/WordNetPartOfSpeech.java
@@ -0,0 +1,12 @@
+package nlpstudio.tools.wordnet;
+
+/**
+ * Created by Yuhuan Jiang (jyuhuan@gmail.com) on 5/16/15.
+ */
+public enum WordNetPartOfSpeech {
+    Noun,
+    Verb,
+    Adverb,
+    Adjective,
+    AdjectiveSatellite
+}
