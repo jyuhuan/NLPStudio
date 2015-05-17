1fd7807daa97a92a52c8eb01db47f7efdc453552
yuhuan <jyuhuan@gmail.com>
2015-05-17 18:53:19 -0400

    renamed WordNet to WordNetReader

---
+++ src/main/scala/nlpstudio/tools/wordnet/WordNetReader.scala
@@ -0,0 +1,97 @@
+package nlpstudio.tools.wordnet
+import scala.collection.JavaConversions._
+
+import java.io.File
+import java.net.URL
+
+import edu.mit.jwi.Dictionary
+import edu.mit.jwi.item._
+import nlpstudio.core._
+import nlpstudio.core.GlobalCodebooks._
+
+
+/**
+ * Created by Yuhuan Jiang (jyuhuan@gmail.com) on 5/16/15.
+ */
+class WordNetReader(wordNetHome: String) {
+  val path = wordNetHome + File.separator + "dict"
+  val url = new URL("file", null, path)
+  val dict = new Dictionary(url)
+  dict.open()
+
+  private def pos2pos(pos: WordNetPartOfSpeech): POS = {
+    if (pos == WordNetPartOfSpeech.Noun) POS.NOUN
+    else if (pos == WordNetPartOfSpeech.Noun) POS.NOUN
+    else if (pos == WordNetPartOfSpeech.Noun) POS.NOUN
+    else if (pos == WordNetPartOfSpeech.Noun) POS.NOUN
+    else if (pos == WordNetPartOfSpeech.Noun) POS.NOUN
+    else null
+  }
+
+  def hypernymsOf(word: Int, partOfSpeech: WordNetPartOfSpeech): Array[Phrase] = {
+    try {
+      val wordnetPos = pos2pos(partOfSpeech)
+
+      val indexWord = dict.getIndexWord(cb(word), wordnetPos)
+      val wordId = indexWord.getWordIDs.head
+      val wordnetWord = dict.getWord(wordId)
+      val synset = wordnetWord.getSynset
+      val hypernyms = synset.getRelatedSynsets(Pointer.HYPERNYM)
+
+      hypernyms.flatMap(h ⇒ dict.getSynset(h).getWords.map(w ⇒ {
+        val lemma = w.getLemma
+        val words = lemma.split('_').map(w ⇒ cb(w))
+        Phrase(words)
+      })).toArray
+    }
+    catch {
+      case e: Exception ⇒ Array[Phrase]()
+    }
+  }
+
+  def hyponymsOf(word: Int, partOfSpeech: WordNetPartOfSpeech): Array[Phrase] = {
+    try {
+      val wordnetPos = pos2pos(partOfSpeech)
+
+      val indexWord = dict.getIndexWord(cb(word), wordnetPos)
+      val wordId = indexWord.getWordIDs.head
+      val wordnetWord = dict.getWord(wordId)
+      val synset = wordnetWord.getSynset
+      val hypernyms = synset.getRelatedSynsets(Pointer.HYPONYM)
+
+      hypernyms.flatMap(h ⇒ dict.getSynset(h).getWords.map(w ⇒ {
+        val lemma = w.getLemma
+        val words = lemma.split('_').map(w ⇒ cb(w))
+        Phrase(words)
+      })).toArray
+    }
+    catch {
+      case e: Exception ⇒ Array[Phrase]()
+    }
+  }
+
+  def synonymsOf(word: Int, partOfSpeech: WordNetPartOfSpeech): Array[Phrase] = {
+    try {
+      val wordnetPos = pos2pos(partOfSpeech)
+
+      val indexWord = dict.getIndexWord(cb(word), wordnetPos)
+      val wordId = indexWord.getWordIDs.head
+      val wordnetWord = dict.getWord(wordId)
+      val synset = wordnetWord.getSynset
+
+      synset.getWords.map(w ⇒ {
+        val lemma = w.getLemma
+        val words = lemma.split('_').map(w ⇒ cb(w))
+        Phrase(words)
+      }).toArray
+    }
+    catch {
+      case e: Exception ⇒ Array[Phrase]()
+    }
+  }
+}
+
+
+object WordNetReader {
+  val defaultHome = "/usr/local/WordNet-3.0"
+}
