5ce48fe229e3f98c17af64c977aad3b2238240b8
yuhuan <jyuhuan@gmail.com>
2015-05-19 14:47:13 -0400

    the trait Feature now has a required name method

--- src/main/scala/learning/feature/Feature.scala
+++ src/main/scala/learning/feature/Feature.scala
@@ -6,6 +6,8 @@ import foundation.math.linearalgebra.core.SparseVector
  * Created by Yuhuan Jiang (jyuhuan@gmail.com) on 3/24/15.
  */
 trait Feature {
+  def name: Int
+
   def valueAsString = {
     if (this.isInstanceOf[CategoricalFeature]) {
       this.asInstanceOf[CategoricalFeature].value
